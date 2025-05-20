f344b42c519c012d15ae864f120803fc02e448f7
AyaAbrada <abradaaya2@gmail.com>
2025-05-20 12:32:11 +0200

    first commit

---
+++ src/main/java/com/example/reservation_des_evenements/ReservationDesEvenementsApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservation_des_evenements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationDesEvenementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationDesEvenementsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reservation_des_evenements/ReservationDesEvenementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservation_des_evenements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationDesEvenementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
